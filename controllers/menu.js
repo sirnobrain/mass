65b0171b20d24a1e91b8c0f725c91745fa7a5e85
Dimitri <deathmitri@gmail.com>
2017-10-10 15:53:49 +0700

    Added controllers template

---
+++ controllers/menu.js
@@ -0,0 +1,19 @@
+const Model = require('../models');
+
+class MenuController {
+
+  static create(obj, req, res) {
+
+  }
+
+  static update(obj, id, req, res) {
+
+  }
+
+  static delete(obj, id, req, res) {
+
+  }
+
+}
+
+module.exports = MenuController;
