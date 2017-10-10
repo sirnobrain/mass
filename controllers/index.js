6e8da4f0237ecb5aa68b64102009677e96046a3d
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-10-10 18:11:25 +0700

    finish showAvailableTables() in index controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,19 @@
 const Table = require('./../models/table');
 
 class Index {
-	static showAvailableTables() {
+	// query available tables and send the result to view
+	static showAvailableTables(req, res) {
 		const options = {where: {isEmpty: true}};
+
 		Table.findAll(options)
 		.then(emptyTables => {
+			const viewData = {
+				data: emptyTables,
+				err: null,
+				session: req.sessionRole
+			};
 
+			res.render('index', viewData);
 		})
 		.catch(err => {
 			if (err) throw err;
