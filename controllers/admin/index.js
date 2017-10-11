84dc9b546a4d2c489c55addddc21145333551fea
Dimitri <deathmitri@gmail.com>
2017-10-11 19:54:29 +0700

    Counter on dashboard cards fixed

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -1,10 +1,42 @@
+const Model = require('../../models');
+
 class AdminController {
 
   static index(req, res) {
 
-    res.render('admin/dashboard/index', {
-      pageTitle: 'Admin Dashboard',
-      session: req.session
+    Promise.all([
+      Model.Menu.count(),
+      Model.Table.count(),
+      Model.User.count(),
+    ]).then((values) => {
+
+      let counter = {};
+
+      counter.menus = values[0];
+      counter.tables = values[1];
+      counter.users = values[2];
+
+      let whereOptions = {};
+      let from = new Date();
+          from.setDate(from.getDate() - 1);
+      let fromPlusOne = new Date();
+
+      whereOptions.createdAt = { $gte: from, $lt: fromPlusOne };
+
+      // TODO: BEST SELLER
+      // Model.Order.findAll({
+      //   include: [ Model.Menu ],
+      //   where: whereOptions
+      // }).then((orders) => {
+      //
+      // });
+
+      res.render('admin/dashboard/index', {
+        pageTitle: 'Admin Dashboard',
+        session: req.session,
+        counter: counter
+      });
+
     });
 
   }
