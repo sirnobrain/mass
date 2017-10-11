ba6dd04151871b933db8c699d3df22b64717054c
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-10-11 10:42:16 +0700

    create kitchen controller's skeleton--kitchen controllers not done yet

---
+++ controllers/kitchen.js
@@ -0,0 +1,15 @@
+const Model = require('./../models');
+
+class Kitchen {
+
+	// query active orders and display them on kitchen screen
+	static showActiveOrders(req, res) {
+
+	}
+
+	static updateBatchStatus(req, res) {
+
+	}
+}
+
+module.exports = Kitchen;
